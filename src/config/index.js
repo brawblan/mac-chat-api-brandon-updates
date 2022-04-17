01f34b2bf4d0ac80886228651fb6e0fbfd00d34b
brandonblankenstein <brandon.blankenstein@gmail.com>
2022-04-16 19:47:36 -0500

    updated mongoUrl and port

--- src/config/index.js
+++ src/config/index.js
@@ -1,10 +1,10 @@
 import mongodb from 'mongodb';
 
 export default {
-  "port": 3005,
-  "mongoUrl": "mongodb://localhost:27017/chat-api",
-  // "port": process.env.PORT,
-  // "mongoUrl": process.env.MONGODB_URI,
+  // "port": 3005,
+  // "mongoUrl": "mongodb://localhost:27017/chat-api",
+  "port": process.env.PORT,
+  "mongoUrl": process.env.MONGODB_URI,
   "bodyLimit": "100kb"
 }
 
